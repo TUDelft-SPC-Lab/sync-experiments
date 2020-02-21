1c31f7d9162c53f32719c2cf2e8db0f88b72e81b
Stephanie Tan <stephanietan0514@gmail.com>
2020-02-21 16:43:00 +0100

    code for manual demod

--- code/manualDemodSignal.m
+++ code/manualDemodSignal.m
@@ -4,17 +4,33 @@ close all
 
 rng(1)
 
-data = audioread('Test1.wav');
-dataChan1 = data(:,1);
-dataChan2 = data(:,2);
-[shiftedDataChan,shift,binaryStream] = processRawSignal(dataChan1);
-% syncwordIdxStart = findSyncWord(binaryStream);
-% syncWord = '0011111111111101';
-% binaryStream = binaryStream((syncwordIdxStart(1)+length(syncWord)):end);
-csvwrite('binaryStream.csv',binaryStream)
+data = audioread('run1.wav');
+dataChan1 = data((192000*5+1):192000*6,1);
 
-function [shiftedDataChan,shift,binaryStream] = processRawSignal(dataChan)
-    dataChan(dataChan>0)=1;
+dataChan1(dataChan1>0)=1;
+dataChan1(dataChan1<0)=-1;
+idx = find(dataChan1==1,1);
+dataChan1=dataChan1(idx:end);
+
+leftover = mod(length(dataChan1),80);
+dataChan1 = dataChan1(1:(length(dataChan1)-leftover));
+
+dataChan1Reshaped = reshape(dataChan1,80,[]);
+dataChan1ReshapedFirst = dataChan1Reshaped(1:40,:)*1;
+dataChan1ReshapedSecond = dataChan1Reshaped(41:end,:)*(-1);
+dataChan1Multiplied = [dataChan1ReshapedFirst;dataChan1ReshapedSecond];
+dataChan1Summed = sum(dataChan1Multiplied,1);
+
+
+% [shiftedDataChan,shift,binaryStream,count] = processRawSignal(dataChan1);
+syncWord = '0011111111111101';
+
+% syncwordIdxStart = findSyncWord(binaryStream,syncWord);
+% binaryStream = binaryStream((syncwordIdxStart(1)+length(syncWord)):end);
+% csvwrite('binaryStream.csv',binaryStream)
+
+function [shiftedDataChan,shift,binaryStream,count] = processRawSignal(dataChan)
+    dataChan(dataChan>=0)=1;
     dataChan(dataChan<0)=-1;
     thresholdedDataChan = dataChan;
     binaryStream = [];
@@ -25,14 +41,20 @@ function [shiftedDataChan,shift,binaryStream] = processRawSignal(dataChan)
     
     % 80 samples per bit
     i=1;
+    count = 0;
     while (i<(length(shiftedDataChan)-mod(length(shiftedDataChan),80)))
         startIndex = i;
         endIndex = i+79;
         chunk = shiftedDataChan(startIndex:endIndex);
+        chunk(1:40) = chunk(1:40)*1;
+        chunk(41:end) = chunk(41:end)*(-1);
         if ((sum(chunk)/length(chunk))==1)
             binaryStream = [binaryStream;ones(80,1)];
         elseif ((sum(chunk)/length(chunk))==0)
             binaryStream=[binaryStream;zeros(80,1)];
+        % there are other possible values of the sum
+        else
+            count = count +1;
         end
         i = endIndex+1;
     end
