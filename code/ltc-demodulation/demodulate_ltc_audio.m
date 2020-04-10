cad8d5dbce1b4080737b1e76759a721fd6caa17c
Chirag Raman <chirag.raman@gmail.com>
2020-04-10 12:37:36 +0200

    Rename file to `demodulate_ltc_audio`

--- code/ltc-demodulation/demodulate_ltc_audio.m
+++ code/ltc-demodulation/demodulate_ltc_audio.m
@@ -1,4 +1,4 @@
-function [] = binarize_ltc_audio(audio_file, out_file)
+function [] = demodulate_ltc_audio(audio_file, out_file)
 
     [data, fs] = audioread(audio_file);
     NSAMPSPERBIT = 80;
@@ -15,25 +15,25 @@ function [] = binarize_ltc_audio(audio_file, out_file)
         [m, idmax] = max(channel(1:NSAMPSPERBIT));
         idx = min(idmin, idmax);
 
-        decoded = [];
+        demodulated = [];
 
         while idx < size(data, 1)-NSAMPSPERBIT
             % Check for sign at samples 20 and 60 in the period
             bit = ~isequal(sign(channel(idx+20)), sign(channel(idx+60)));
-            decoded = [decoded, bit];
+            demodulated = [demodulated, bit];
             % Find the peak within a tolerance of 6 samples
             [m, idt] = max(abs(channel(idx+77:idx+83)));
             idx = idx + 77 + idt - 1;
         end
 
         % Convert to string and write to disk
-        decoded_str = num2str(decoded, '%d');
-        fprintf(file_handle, '%s\n', decoded_str);
+        demodulated_str = num2str(demodulated, '%d');
+        fprintf(file_handle, '%s\n', demodulated_str);
 
         % Sanity check that the frame numbers are the same - python code
         % Find the location of the sync words
         % pattern = '0011111111111101';
-        % strfind(decoded_str, pattern)
+        % strfind(demodulated_str, pattern)
     end
 
     % Close the file handle
